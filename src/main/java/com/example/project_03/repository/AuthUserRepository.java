0699bad5e45a289b0af08871674ff5b1bbedf0b1
Miyuranga305 <105289581+Miyuranga305@users.noreply.github.com>
2024-02-23 11:28:54 +0530

    Add files via upload

---
+++ src/main/java/com/example/project_03/repository/AuthUserRepository.java
@@ -0,0 +1,12 @@
+package com.example.project_03.repository;
+
+import com.example.project_03.entity.AuthUser;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AuthUserRepository extends MongoRepository<AuthUser, String> {
+    Optional<AuthUser> findByUsername(String username);
+}
